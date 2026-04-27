ba61645efc047ac1b48647c9b01ec0f233623753
Mbiamou Richy <richymbiamou52@gmail.com>
2026-04-27 02:49:58 +0100

    update de header

--- components/header.tsx
+++ components/header.tsx
@@ -70,7 +70,7 @@ export function Header() {
         <div className="hidden md:flex h-4 flex-row justify-center gap-2 items-center">
           <ModeToggle screen="desktop" />
           <Separator orientation="vertical" />
-          <AutomatiserButton href="/test" text="Tester sur Whatsapp" />
+          <AutomatiserButton text="Tester sur Whatsapp" />
         </div>
         <MobileNav />
       </nav>
